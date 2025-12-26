fe028a70d922a4c317b2762b9dd54b1fc39a40ff
kstutz <github@kathistutz.de>
2025-12-26 00:03:31 +0000

    One instruction, several ingredients per step

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from .models import Recipe, IngredientInRecipe, RecipeUtensil
+from .models import Recipe, StepIngredient, RecipeUtensil
 
 
 class IndexView(generic.ListView):
@@ -14,8 +14,11 @@ class DetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['ingredients'] = IngredientInRecipe.objects.filter(recipe=self.object)
-        context['instructions'] = self.object.instruction_set.order_by('step_number')
+        steps = self.object.step_set.order_by('number')
+        for step in steps:
+            step.ingredients = StepIngredient.objects.filter(step=step)
+        context['steps'] = steps
+#        context['instructions'] = self.object.instruction_set.order_by('step_number')
 #        context['utensils'] = self.object.recipeutensil_set.all()
         context['utensils'] = RecipeUtensil.objects.filter(recipe=self.object)
         return context
